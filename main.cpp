207f2f293c1161fb7e1931c90695ffc4c35e2a52
Kopylov Nick <www.ves66a96@mail.ru>
2015-12-19 22:25:05 +0300

    Correct layout

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w("192.168.10.4", 2323);
+    MainWindow w("localhost", 2323);
     w.show();
 
     return a.exec();
